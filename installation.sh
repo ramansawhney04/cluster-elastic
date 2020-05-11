b8107c27737d62e68821fed2b1f5c6423ce2dd40
Raman Sawhney <ramansawhney04@gmail.com>
2020-05-11 07:07:48 -0400

    master-1

---
+++ installation.sh
@@ -0,0 +1,4 @@
+curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.2.1-linux-x86_64.tar.gz
+tar -xzvf elasticsearch-7.2.1-linux-x86_64.tar.gz
+rm elasticsearch-7.2.1-linux-x86_64.tar.gz
+mv elasticsearch-7.2.1 elasticsearch
