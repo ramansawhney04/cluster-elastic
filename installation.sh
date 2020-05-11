79e1fb2d3f5fe75acff7e129ea2d674fec0da37d
ramansawhney04 <ramansawhney04@gmail.com>
2020-05-11 17:22:41 +0530

    Delete installation.sh

--- installation.sh
+++
@@ -1,4 +0,0 @@
-curl -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.2.1-linux-x86_64.tar.gz
-tar -xzvf elasticsearch-7.2.1-linux-x86_64.tar.gz
-rm elasticsearch-7.2.1-linux-x86_64.tar.gz
-mv elasticsearch-7.2.1 elasticsearch
